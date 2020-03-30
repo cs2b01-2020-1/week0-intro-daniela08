4eb13c21f73ae34828025ff3ff758b34343ca6b7
daniela08 <danielacauti@gmail.com>
2020-03-29 22:18:36 -0500

    Covid

---
+++ covid.py
@@ -0,0 +1,16 @@
+genoma1 = "AY274119.txt"
+genoma2 = "AY278488.2.txt"
+genoma3 = "MN908947.txt"
+genoma4 = "MN988668.txt"
+genoma5 = "MN988669.txt"
+
+g1 = open(genoma1)
+g2 = open(genoma2)
+g3 = open(genoma3)
+g4 = open(genoma4)
+g5 = open(genoma5)
+g1.close()
+g2.close()
+g3.close()
+g4.close()
+g5.close()
