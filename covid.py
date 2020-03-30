670057efd8df1ce47555f2b402146fa0b6e7f1f1
Daniela <52041953+daniela08@users.noreply.github.com>
2020-03-29 23:15:07 -0500

    Update covid.py

--- covid.py
+++ covid.py
@@ -1,16 +1,10 @@
+import os
+
 genoma1 = "AY274119.txt"
 genoma2 = "AY278488.2.txt"
 genoma3 = "MN908947.txt"
 genoma4 = "MN988668.txt"
 genoma5 = "MN988669.txt"
 
-g1 = open(genoma1)
-g2 = open(genoma2)
-g3 = open(genoma3)
-g4 = open(genoma4)
-g5 = open(genoma5)
-g1.close()
-g2.close()
-g3.close()
-g4.close()
-g5.close()
+files = [genoma1, genoma2, genoma3, genoma4, genoma5]
+matrix = [[0]*(len(files)+1) for i in range(len(files)+1)]
